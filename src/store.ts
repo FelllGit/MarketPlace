5ee2b5f78e30235ce39aed34e46848e3e907d606
Andrii <andriy30534>
2023-11-03 18:32:45 +0200

    some fix

--- src/store.ts
+++ src/store.ts
@@ -1,30 +1,30 @@
-import { create } from 'zustand';
+import { create } from 'zustand'
 
 interface IExample {
-  counter: number;
-  increment: () => void;
-  decrement: () => void;
-  catFact: string | undefined;
-  setCatFact: (fact: string) => void;
-  catFactFetcher: () => Promise<string>;
+	counter: number
+	increment: () => void
+	decrement: () => void
+	catFact: string | undefined
+	setCatFact: (fact: string) => void
+	catFactFetcher: () => Promise<string>
 }
 
 // Create the store
 const useStore = create<IExample>((set) => ({
-  // States
-  counter: 0,
-  catFact: undefined,
+	// States
+	counter: 0,
+	catFact: undefined,
 
-  // Actions
-  increment: () => set((state) => ({ counter: state.counter + 1 })),
-  decrement: () => set((state) => ({ counter: state.counter - 1 })),
-  setCatFact: (fact) => set({ catFact: fact }),
-  // Fetching
-  catFactFetcher: async () => {
-    const response = await fetch('https://catfact.ninja/fact');
-    const data = await response.json();
-    return data.fact;
-  },
-}));
+	// Actions
+	increment: () => set((state) => ({ counter: state.counter + 1 })),
+	decrement: () => set((state) => ({ counter: state.counter - 1 })),
+	setCatFact: (fact) => set({ catFact: fact }),
+	// Fetching
+	catFactFetcher: async () => {
+		const response = await fetch('https://catfact.ninja/fact')
+		const data = await response.json()
+		return data.fact
+	},
+}))
 
-export default useStore;
+export default useStore
